1934e31cdfdbb52bd369d332cb93abe59f0f2c6e
Aryan <arya@ecs.vuw.ac.nz>
2025-06-23 17:51:37 +1200

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Aryan Singh on 23/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjectTests/ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTests.swift
+//  ProjectTests
+//
+//  Created by Aryan Singh on 23/06/25.
+//
+
+import Testing
+@testable import Project
+
+struct ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
